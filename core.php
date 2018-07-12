058ede41c6e9b5de9ee49ccfe1a256bf6d617598
feedmenoise <feedmenoise@list.ru>
2018-07-12 14:34:31 +0300

    added functional for overwrite changes in /etc/asterisk/support.conf

--- core.php
+++ core.php
@@ -1,17 +1,19 @@
 
 <?php
-$secret = $_POST['secret'];
-$ar=explode(',',$secret);
+#include mbstring.h
+function write () {
+    $secret = $_POST['secret'];
+    $ar=explode(',',$secret);
 
-foreach ($ar as $value) {
+    foreach ($ar as $value) {
 
-    $toDelete = 3;
-    mb_internal_encoding("UTF-8");
-    $result = mb_substr( $value, $toDelete); // глу скребёт мышь
+        $toDelete = 3;
+        mb_internal_encoding("UTF-8");
+        $result = mb_substr( $value, $toDelete);
 
-    $temp = "exten => s,n,Playback(" . $result . ")" . "<br>";
-    print ($temp);
-}
+        $temp = "exten => s,n,Playback(" . $result . ")" . "<br>";
+        print ($temp);
+    }
 
     $fp = fopen('support.conf', 'w');
     foreach ($ar as $value) {
@@ -24,5 +26,25 @@ foreach ($ar as $value) {
         fwrite($fp, $temp);
     }
     fclose($fp);
+}
+?>
+
+
+<a href="?confirm=true"><input name="confirm" type="button" value="confirm" " /></a>
+
+<?php
+function confirm() {
+    $message=shell_exec("./testscript.sh 2>&1");
+    print_r($message);
+}
+
+if (isset($_GET['confirm'])) {
+    confirm();
+}
+
+if (isset($_GET['write'])){
+    write();
+}
 
 ?>
+
